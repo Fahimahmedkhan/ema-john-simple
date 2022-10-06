17a4c8efba51abaaf2a7d2e89ef65510debeaddd
Fahim Ahmed Khan Gorbo <53188460+Fahimahmedkhan@users.noreply.github.com>
2022-10-06 23:45:34 +0600

    products-saved cartLoader & return multiple-Items

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Inventory from './components/Inventory/Inventory';
 import Orders from './components/Orders/Orders';
 import Shop from './components/Shop/Shop';
 import Main from './layouts/Main';
+import { productsAndCartLoader } from './loaders/productsAndCartLoader';
 
 function App() {
   const router = createBrowserRouter([
@@ -19,9 +20,7 @@ function App() {
         },
         {
           path: '/orders',
-          loader: () => {
-            return fetch('products.json')
-          },
+          loader: productsAndCartLoader,
           element: <Orders></Orders>
         },
         {
