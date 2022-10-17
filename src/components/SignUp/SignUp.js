8dec35a1a13f544c5d0f9cd52aca717d99e73261
fahimahmedkhan <rolx9999@gmail.com>
2022-10-17 19:56:42 +0600

    Implement Login Sign Up and Sign Out

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/UserContext';
 import './SignUp.css';
 
 const SignUp = () => {
+
+    const [error, setError] = useState(null);
+    const { createUser } = useContext(AuthContext);
+
+    const handelSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+
+        if (password.length < 6) {
+            setError('Password should be 6 characters or more.');
+            return;
+        }
+        if (password !== confirm) {
+            setError('Your password did not match.');
+            return;
+        }
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset();
+            })
+            .catch(error => console.error(error))
+    }
+
     return (
         <div className='form-container'>
             <h1 className='form-title'>SignUp</h1>
-            <form>
+            <form onSubmit={handelSubmit}>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" placeholder='Please Enter Your Email.' required />
@@ -19,9 +49,10 @@ const SignUp = () => {
                     <label htmlFor="confirm">Confirm Password</label>
                     <input type="password" name="confirm" placeholder='Please Enter Your Password.' required />
                 </div>
-                <input className='btn-submit' type="submit" value="Login" />
+                <input className='btn-submit' type="submit" value="SignUp" />
             </form>
             <p>Already Have an Account?<samp><Link to="/login"> Create a New Account</Link></samp></p>
+            <p className='text-error'>{error}</p>
         </div>
     );
 };
