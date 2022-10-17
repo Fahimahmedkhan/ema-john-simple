df2de757ee58581830a01111ca3b8258bdb7bdff
fahimahmedkhan <rolx9999@gmail.com>
2022-10-17 20:48:04 +0600

    Login Redirect Navigate to the right route

---
+++ src/components/Shipping/Shipping.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Shipping.css';
+
+const Shipping = () => {
+    return (
+        <div>
+            <h1>This is shipping</h1>
+        </div>
+    );
+};
+
+export default Shipping;
